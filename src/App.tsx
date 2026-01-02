a6726913db4d97a04f4945ac688ebb09e505e698
Nirmal-Kharal <nirmalkharal40@gmail.com>
2026-01-02 14:09:23 +0545

    TImerDisplay Component  done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./component/Button"
+import TimerDisplay from "./component/TimerDisplay";
 
 const App = () => {
   const math = Math.random();
@@ -6,6 +7,7 @@ const App = () => {
     <div className='bg-primary h-screen'>
     <Button  variant="primary" onClick={() => console.log(math)}>Primary Button</Button>
     <Button  variant="secondary" onClick={() => alert("Clicked Secondary")}>Secondary Button</Button>
+    <TimerDisplay seconds={3600}/>
     </div>
   )
 }
--- src/component/TimerDisplay.tsx
+++ src/component/TimerDisplay.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
+
 interface TimerDisplayProps{
     seconds:number;
 }
 
  function formatTime(totalSeconds:number):string{
 
+const hours = Math.floor(totalSeconds/3600);
+const minutes = Math.floor((totalSeconds % 3600)/60);
+const seconds = totalSeconds %60;
+const pad = (num : number) => num.toString().padStart(2,"0")
+
+return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
  }
 
 const TimerDisplay = ({ seconds}:TimerDisplayProps) => {
   return (
-    <div>TimerDisplay</div>
+      <div className='font-bold text-white text-7xl tracking-wider'>
+        {  formatTime(seconds)}
+      </div>
   )
 }
 
